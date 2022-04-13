db78fb760313bf971f7d750b8443f4e0c082477a
Armando Aguirre <mandoamtz1@gmail.com>
2022-04-13 11:08:04 -0500

    Iniciando el desarrollo del Web Api

---
+++ WebApi/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Entidades;
+
+namespace WebApi
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
---
+++ WebApi/Controllers/AutoresController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
