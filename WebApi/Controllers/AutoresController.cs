88851d9f3448f2f672aab73a61c7e2fbf8121ec8
Armando Aguirre <mandoamtz1@gmail.com>
2022-04-13 11:14:57 -0500

    Leyendo y creando recursos desde el controlador

--- WebApi/Controllers/AutoresController.cs
+++ WebApi/Controllers/AutoresController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Entidades;
 
 namespace WebApi.Controllers
 {
@@ -10,6 +12,20 @@ namespace WebApi.Controllers
         public AutoresController(ApplicationDbContext context)
         {
             this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
