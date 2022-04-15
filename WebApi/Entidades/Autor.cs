6150a5aec19f990bc3befd548929cc3ef9bcd559
Armando Aguirre <mandoamtz1@gmail.com>
2022-04-15 13:23:06 -0500

    add: Validaciones por modelo

--- WebApi/Entidades/Autor.cs
+++ WebApi/Entidades/Autor.cs
@@ -4,27 +4,50 @@ using WebApi.Validaciones;
 
 namespace WebApi.Entidades
 {
-    public class Autor
+    public class Autor : IValidatableObject
     {
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El campo {0} es requerido.")]
         [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
-        [PrimeraLetraMayuscula]
+        //[PrimeraLetraMayuscula]
         public string Nombre { get; set; }
 
-        [Range(18, 100)]
-        [NotMapped]
-        public int Edad { get; set; }
+        //[Range(18, 100)]
+        //[NotMapped]
+        //public int Edad { get; set; }
 
-        [CreditCard]
-        [NotMapped]
-        public string TarjetaDeCredito { get; set; }
+        //[CreditCard]
+        //[NotMapped]
+        //public string TarjetaDeCredito { get; set; }
 
-        [Url]
-        [NotMapped]
-        public string Url { get; set; }
+        //[Url]
+        //[NotMapped]
+        //public string Url { get; set; }
 
-        public List<Libro> Libros { get; set; }
+        public int Menor { get; set; }
+      public int Mayor { get; set; }
+
+        public List<Libro>? Libros { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayuscula.",
+                        new string[] { nameof(Nombre) });
+                }
+            }
+
+            if (Menor > Mayor)
+            {
+                yield return new ValidationResult("Este valor no puede ser mas grande que el campo Mayor.",
+                    new string[] { nameof(Menor) });
+            }
+        }
     }
 }
--- WebApi/Entidades/Libro.cs
+++ WebApi/Entidades/Libro.cs
@@ -1,10 +1,13 @@
-﻿namespace WebApi.Entidades
+﻿using WebApi.Validaciones;
+
+namespace WebApi.Entidades
 {
     public class Libro
     {
         public int Id { get; set; }
+        [PrimeraLetraMayuscula]
         public string Titulo { get; set; }
         public int AutorId { get; set; }
-        public Autor Autor { get; set; }
+        public Autor? Autor { get; set; }
     }
 }
